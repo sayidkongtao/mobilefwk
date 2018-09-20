2aafb932305d3db75a9d7198c8581668c66346cb
Sayid <kongtao01@beyondsoft.com>
2018-09-20 10:55:41 +0800

    ios Fix issue

--- po/ios/pages/contactdetails.py
+++ po/ios/pages/contactdetails.py
@@ -13,6 +13,16 @@ class ContactDetails(BasePage):
         super(ContactDetails, self).__init__(appium_driver)
 
     @property
+    def title(self):
+        return Utils.find(
+            Text,
+            (MobileBy.XPATH, '//XCUIElementTypeStaticText[@name="联系人详情"]'),
+            "联系人详情"
+            "ContactDetails",
+            self.driver
+        )
+
+    @property
     def name(self):
         return Utils.find(
             Text,
@@ -45,5 +55,8 @@ class ContactDetails(BasePage):
         )
 
     # page logic
+    def at(self):
+        Utils.wait_until_condition(lambda: self.title.is_visible())
+
     def goto_email_history(self):
         self.email_history.click()
